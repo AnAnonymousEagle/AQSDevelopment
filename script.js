d5608f5682df5716d706cb51a0741b962c382054
Aguila <112664301+AnAnonymousEagle@users.noreply.github.com>
2022-10-31 18:54:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,9 +9,31 @@ function scrollFunction() {
     document.getElementById("scrollHeader").classList.add("aboveHeader");
     document.getElementById("scrollLogo").classList.remove("aboveLogo");
     document.getElementById("scrollName").classList.add("aboveName");
+
   } else {
     document.getElementById("scrollHeader").classList.remove("aboveHeader");
     document.getElementById("scrollLogo").classList.add("aboveLogo");
     document.getElementById("scrollName").classList.remove("aboveName");
   }
 }
+
+
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
+
+
+
