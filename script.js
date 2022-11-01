549735f5dbe59eaa2826124f308a92456ff42762
Aguila <112664301+AnAnonymousEagle@users.noreply.github.com>
2022-11-01 16:24:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,31 +9,15 @@ function scrollFunction() {
     document.getElementById("scrollHeader").classList.add("aboveHeader");
     document.getElementById("scrollLogo").classList.remove("aboveLogo");
     document.getElementById("scrollName").classList.add("aboveName");
+    document.getElementById("brand-name").textContent = "Aquila Sprites";
 
   } else {
     document.getElementById("scrollHeader").classList.remove("aboveHeader");
     document.getElementById("scrollLogo").classList.add("aboveLogo");
     document.getElementById("scrollName").classList.remove("aboveName");
+    document.getElementById("brand-name").textContent = "AQS";
   }
 }
-
-
-
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
-
-
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
-
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});
 
 
 
