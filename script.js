8f6399d505550bfcd7760a1dd331476917277b62
Aguila <112664301+AnAnonymousEagle@users.noreply.github.com>
2022-11-02 22:14:30 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
+
+
+
 document.getElementById("scrollLogo").classList.add("aboveLogo");
 
-window.onscroll = function () {
-  scrollFunction();
-};
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+// function scrollFunction() {
+//   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+//     document.getElementById("scrollHeader").classList.add("aboveHeader");
+//     document.getElementById("scrollLogo").classList.remove("aboveLogo");
+//     document.getElementById("scrollName").classList.add("aboveName");
+//     document.getElementById("brand-name").textContent = "Aquila Sprites";
+
+//   } else {
+//     document.getElementById("scrollHeader").classList.remove("aboveHeader");
+//     document.getElementById("scrollLogo").classList.add("aboveLogo");
+//     document.getElementById("scrollName").classList.remove("aboveName");
+//     document.getElementById("brand-name").textContent = "AQS";
+//   }
+// }
+
+
+
+
+window.addEventListener("scroll", function() {
+  var elementTarget = document.getElementById("section1");
+  if (window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight)) {
     document.getElementById("scrollHeader").classList.add("aboveHeader");
     document.getElementById("scrollLogo").classList.remove("aboveLogo");
     document.getElementById("scrollName").classList.add("aboveName");
     document.getElementById("brand-name").textContent = "Aquila Sprites";
-
+    document.getElementById("socialsBar").style.left = "0";
+    $(".circles-from-right").empty();
+    $(".circles-from-left").empty();
   } else {
     document.getElementById("scrollHeader").classList.remove("aboveHeader");
     document.getElementById("scrollLogo").classList.add("aboveLogo");
     document.getElementById("scrollName").classList.remove("aboveName");
     document.getElementById("brand-name").textContent = "AQS";
+    document.getElementById("socialsBar").style.left = "-5vw";
+    $(".circles-from-right").append("<li>");
+    $(".circles-from-left").append("<li>");
   }
-}
-
-
-
+});
